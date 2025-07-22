7e0058118ad151af5d6f6648c4d8b75dac52ef5a
Maisrousan <maisrousan11@gmail.com>
2025-07-22 18:02:17 +0300

    update

--- src/project10/Mytestcases.java
+++ src/project10/Mytestcases.java
@@ -1,5 +1,6 @@
 package project10;
 
+import java.io.DataInput;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -17,175 +19,214 @@ import org.testng.annotations.Test;
 
 public class Mytestcases {
 
-	WebDriver driver = new 	ChromeDriver();
-	
+	WebDriver driver = new ChromeDriver();
+
 	String URL = "https://codenboxautomationlab.com/practice/";
-	
+
 	@BeforeTest
-	
-	public void MyTestSetUp () {
-		
-		
+
+	public void MyTestSetUp() {
+
 		driver.get(URL);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		
+
 	}
-		
-		
-	
 
+	@Test(enabled = false)
 
-		@Test (enabled=false)
-		
-		public void RadioButton () {
-			
-			WebElement ContainerRadioButton = driver.findElement(By.xpath("//div[@id='radio-btn-example']//fieldset"));
-			ContainerRadioButton.findElements(By.tagName("input")).get(0).click();
-			
-			
-		}		
-		
-		
-		
-		@Test (enabled=false)
-		
-		public void DynamicDropdown() throws InterruptedException {
-			
-			
-			String [] Countries = { "Jor" , "Uni" , "Ir" ,"Syr"  };
-			
-			WebElement  SelectCountry = driver.findElement(By.id("autocomplete"));
-			SelectCountry.sendKeys(Countries[2]);
-			Thread.sleep(1000);
-			SelectCountry.sendKeys(Keys.chord(Keys.ARROW_DOWN , Keys.ARROW_DOWN , Keys.ENTER));
-			
-			
-			
-			
+	public void RadioButton() {
+
+		WebElement ContainerRadioButton = driver.findElement(By.xpath("//div[@id='radio-btn-example']//fieldset"));
+		ContainerRadioButton.findElements(By.tagName("input")).get(0).click();
+
+	}
+
+	@Test(enabled = false)
+
+	public void DynamicDropdown() throws InterruptedException {
+
+		String[] Countries = { "Jor", "Uni", "Ir", "Syr" };
+
+		WebElement SelectCountry = driver.findElement(By.id("autocomplete"));
+		SelectCountry.sendKeys(Countries[2]);
+		Thread.sleep(1000);
+		SelectCountry.sendKeys(Keys.chord(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER));
+
+	}
+
+	@Test(enabled = false)
+	public void StaticDropdown() {
+		WebElement MySelectTag = driver.findElement(By.id("dropdown-class-example"));
+		Select MySelect = new Select(MySelectTag);
+
+		// MySelect.selectByContainsVisibleText("API");
+		MySelect.selectByValue("option2");
+		// MySelect.selectByIndex(1);
+
+	}
+
+	@Test(enabled = false)
+
+	public void CheckBox() {
+
+		WebElement CheckBoxContainer = driver.findElement(By.xpath("//div[@id='checkbox-example']//fieldset"));
+
+		List<WebElement> AllCheckBoxes = CheckBoxContainer.findElements(By.tagName("input"));
+
+		for (int i = 0; i < AllCheckBoxes.size(); i++) {
+
+			AllCheckBoxes.get(i).click();
+
 		}
-		
-		@Test (enabled=false) 
-		public void StaticDropdown () {
-			WebElement MySelectTag = driver.findElement(By.id("dropdown-class-example"));
-			Select MySelect = new Select(MySelectTag) ;
-			
-			
-			//MySelect.selectByContainsVisibleText("API");
-			MySelect.selectByValue("option2");
-			//MySelect.selectByIndex(1);
-			
-			
-			
-			
-			
+
+	}
+
+	@Test(enabled = false)
+
+	public void SwitchWindow() {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,700)");
+
+		WebElement OpenWindow = driver.findElement(By.id("openwindow"));
+		OpenWindow.click();
+
+		System.out.println(driver.getTitle());
+
+		Set<String> Handles = driver.getWindowHandles();
+
+		List<String> AllTabs = new ArrayList<>(Handles);
+
+		driver.switchTo().window(AllTabs.get(1));
+
+		driver.findElement(By.xpath("//li[@id='menu-item-9680']//a")).click();
+		driver.switchTo().window(AllTabs.get(0));
+		System.out.println(driver.getTitle());
+
+	}
+
+	@Test(enabled = false)
+	public void SwitchTab() throws InterruptedException {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,700)");
+
+		WebElement OpenTab = driver.findElement(By.id("opentab"));
+		OpenTab.click();
+
+		System.out.println(driver.getTitle());
+
+		Set<String> Handles = driver.getWindowHandles();
+
+		List<String> AllTabs = new ArrayList<>(Handles);
+
+		driver.switchTo().window(AllTabs.get(1));
+
+		Thread.sleep(2000);
+		System.out.println(driver.getTitle());
+
+	}
+
+	@Test(enabled = false)
+	public void SwitchToAlert() throws InterruptedException {
+
+		WebElement NameInput = driver.findElement(By.id("name"));
+		NameInput.sendKeys("Mais");
+		WebElement AlertButton = driver.findElement(By.id("alertbtn"));
+		AlertButton.click();
+
+		boolean Actual = driver.switchTo().alert().getText().contains("Mais");
+
+		Assert.assertTrue(Actual, "This test for alert");
+
+		Thread.sleep(1000);
+		driver.switchTo().alert().accept();
+
+	}
+
+	@Test(enabled = false)
+	public void WebTable() {
+
+		WebElement TheTable = driver.findElement(By.id("product"));
+		List<WebElement> AllData = TheTable.findElements(By.tagName("td"));
+		// List<WebElement> AllData = TheTable.findElements(By.tagName("th"));
+		// List<WebElement> AllData = TheTable.findElements(By.tagName("tr"));
+
+		for (int i = 5; i < AllData.size(); i = i + 3) {
+			System.out.println(AllData.get(i).getText());
+
 		}
-		
-		
-		
-		@Test (enabled=false)
-		
-		public void CheckBox () {
-			
-			WebElement CheckBoxContainer = driver.findElement(By.xpath("//div[@id='checkbox-example']//fieldset"));
-			
-	
-			
-			List<WebElement> AllCheckBoxes = CheckBoxContainer.findElements(By.tagName("input"));
-			
-			for (int i=0 ; i<AllCheckBoxes.size() ; i++) {
-				
-				
-				AllCheckBoxes.get(i).click();
-				
-			}
-			
-		}
-	
-		
-		
-		@Test (enabled=false)
-		
-		public void SwitchWindow () {
-			
-			
-			JavascriptExecutor js = (JavascriptExecutor) driver ;
-			js.executeScript("window.scrollTo(0,700)");
-			
-			WebElement OpenWindow = driver.findElement(By.id("openwindow"));
-			OpenWindow.click();
-			
-			System.out.println(driver.getTitle());
-			
-			Set<String> Handles = driver.getWindowHandles();
-			
-			List<String> AllTabs = new ArrayList<>(Handles);
-			
-			driver.switchTo().window(AllTabs.get(1));
-			
-			driver.findElement(By.xpath("//li[@id='menu-item-9680']//a")).click();
-			driver.switchTo().window(AllTabs.get(0));
-			System.out.println(driver.getTitle());
-			
-		}
-		
-		
-		@Test (enabled = false)
-		public void SwitchTab () throws InterruptedException {
-			
-			JavascriptExecutor js = (JavascriptExecutor) driver ;
-			js.executeScript("window.scrollTo(0,700)");
-			
-			WebElement OpenTab = driver.findElement(By.id("opentab"));
-			OpenTab.click();
-			
-			System.out.println(driver.getTitle());
-			
-			Set<String> Handles = driver.getWindowHandles();
-			
-			List<String> AllTabs = new ArrayList<>(Handles);
-			
-			driver.switchTo().window(AllTabs.get(1));
-			
-			Thread.sleep(2000);
-			System.out.println(driver.getTitle());
-			
-			
-		}
-		
-		
-		
-		
-		@Test (enabled = false)
-		public void SwitchToAlert () throws InterruptedException {
-			
-			WebElement NameInput = driver.findElement(By.id("name"));
-			NameInput.sendKeys("Mais");
-			WebElement AlertButton = driver.findElement(By.id("alertbtn"));
-			AlertButton.click();
-			
-			
-			boolean Actual = driver.switchTo().alert().getText().contains("Mais");
-			
-			Assert.assertTrue(Actual, "This test for alert");
-			
-			
-			Thread.sleep(1000);
-			driver.switchTo().alert().accept();
-			
-			
-		}
-		
-		
+	}
+
+	@Test(enabled = false)
+
+	public void ElementDisplayed() {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,1500)");
+
+		WebElement InputField = driver.findElement(By.id("displayed-text"));
+		WebElement HideButton = driver.findElement(By.id("hide-textbox"));
+		HideButton.click();
+
+		System.out.println(InputField.isDisplayed());
+
+		WebElement ShowButton = driver.findElement(By.id("show-textbox"));
+
+		ShowButton.click();
+
+		System.out.println(InputField.isDisplayed());
+
+	}
+
+	@Test(enabled = false)
+	public void EnabledDisabled() {
+
+		WebElement DisablesButton = driver.findElement(By.id("disabled-button"));
+		DisablesButton.click();
+
+		WebElement InputField = driver.findElement(By.id("enabled-example-input"));
+		System.out.println(InputField.isEnabled());
+
+		WebElement EnabledButton = driver.findElement(By.id("enabled-button"));
+		EnabledButton.click();
+		System.out.println(InputField.isEnabled());
+
+	}
+
+	@Test(enabled = false)
+	public void MouseHover() {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,1900)");
+
+		WebElement MouseHoverButton = driver.findElement(By.id("mousehover"));
+
+		Actions action = new Actions(driver);
+		action.moveToElement(MouseHoverButton).build().perform();
+
+//		WebElement Top = driver.findElement(By.linkText("Top"));
+//		Top.click();
+		WebElement Reload = driver.findElement(By.linkText("Reload"));
+		Reload.click();
+	}
+
+	@Test
+	public void Calendar() {
+
+		WebElement Calendar = driver.findElement(By.linkText("Booking Calendar"));
+
+		Calendar.click();
+
+		Set<String> Handles = driver.getWindowHandles();
+
+		List<String> AllTabs = new ArrayList<>(Handles);
+
+		driver.switchTo().window(AllTabs.get(1));
+
+		WebElement Day26 = driver.findElement(By.linkText("26"));
+		Day26.click();
+	}
+
 }
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-
